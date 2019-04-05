e4368baced663b7ac63c2759628842dc199d0f1e
tainanboy <frank840925@gmail.com>
2019-04-05 17:01:58 -0400

    update LF2 function, can choose elasticsearch index

--- LF2.py
+++ LF2.py
@@ -10,6 +10,11 @@ from boto3.dynamodb.conditions import Key, Attr
 # 3. formats them and 
 # 4. sends them over text message to the phone number included in the SQS message, using SNS
 
+# choose the elasticsearch index: "restaurants" is the easier version, "predictions" is with the ML service
+#search_index = "restaurants"
+search_index = "predictions"
+
+
 def lambda_handler(event, context):
     # TODO implement
     print("Testing CloudWatch: Call LF2 every minute.")
@@ -54,25 +59,47 @@ def lambda_handler(event, context):
         phone =  message['MessageAttributes']['phone']['StringValue']
         print(location, cuisine, dining_date, dining_time, num_people, phone)
         # use http request to search ElasticSearch index: restaurant
-        # pick a restaurant randomly, get the business_ID (always pick first one here, need further work)
-        r = requests.get('https://search-restaurants-cozvshffzyeezzf7wcfsnab45u.us-east-1.es.amazonaws.com/restaurants/_search?q='+str(cuisine))
-        data = r.json()
-        Business_ID = data['hits']['hits'][0]['_source']['Business_ID']
-        print(Business_ID)
-        # 3. get more information from DynamoDB
-        # search DynamoDB using Business_ID
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('yelp-restaurant')
-        response = table.query(
-            KeyConditionExpression=Key('Business_ID').eq(Business_ID)
-        )
-        # 4. format the message
-        name = response['Items'][0]['Name']
-        address = response['Items'][0]['Address']
-        num_reviews = response['Items'][0]['Num_of_Reviews']
-        rating = response['Items'][0]['Rating']
-        sendMessage = "Hello! For {}, we recommend the {} {} restaurant on {}. The place has {} of reviews and an average score of {} on Yelp. Enjoy!".format(location, name, cuisine, address, num_reviews, rating)
-        print(sendMessage)
+        sendMessage = None
+        if search_index == "restaurants":
+            # pick a restaurant randomly, get the business_ID (always pick first one here, need further work)
+            r = requests.get('https://search-restaurants-cozvshffzyeezzf7wcfsnab45u.us-east-1.es.amazonaws.com/restaurants/_search?q='+str(cuisine))
+            data = r.json()
+            Business_ID = data['hits']['hits'][0]['_source']['Business_ID']
+            print(Business_ID)
+            # 3. get more information from DynamoDB
+            # search DynamoDB using Business_ID
+            dynamodb = boto3.resource('dynamodb')
+            table = dynamodb.Table('yelp-restaurant')
+            response = table.query(
+                KeyConditionExpression=Key('Business_ID').eq(Business_ID)
+            )
+            # 4. format the message
+            name = response['Items'][0]['Name']
+            address = response['Items'][0]['Address']
+            num_reviews = response['Items'][0]['Num_of_Reviews']
+            rating = response['Items'][0]['Rating']
+            sendMessage = "Hello! For {}, we recommend the {} {} restaurant on {}. The place has {} of reviews and an average score of {} on Yelp. Enjoy!".format(location, name, cuisine, address, num_reviews, rating)
+            print(sendMessage)
+        else:
+            # pick a restaurant randomly, get the business_ID (always pick first one here, need further work)
+            r = requests.get('https://search-predictions-nxhiwrjp3ljnqsel5gr6sbxsau.us-east-1.es.amazonaws.com/predictions/_search?q='+str(cuisine))
+            data = r.json()
+            Business_ID = data['hits']['hits'][0]['_source']['Business_ID']
+            print(Business_ID)
+            # 3. get more information from DynamoDB
+            # search DynamoDB using Business_ID
+            dynamodb = boto3.resource('dynamodb')
+            table = dynamodb.Table('yelp-restaurant')
+            response = table.query(
+                KeyConditionExpression=Key('Business_ID').eq(Business_ID)
+            )
+            # 4. format the message
+            name = response['Items'][0]['Name']
+            address = response['Items'][0]['Address']
+            num_reviews = response['Items'][0]['Num_of_Reviews']
+            rating = response['Items'][0]['Rating']
+            sendMessage = "Hello! For {}, we recommend the {} {} restaurant on {}. The place has {} of reviews and an average score of {} on Yelp. Enjoy!".format(location, name, cuisine, address, num_reviews, rating)
+            print(sendMessage)
         # 5. send the message using SNS
         # Create SQS client
         sns = boto3.client('sns')
