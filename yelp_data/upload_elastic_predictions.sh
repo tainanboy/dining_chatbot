017e5bd900d50d0b85f9747d70fee4847a438e2a
tainanboy <frank840925@gmail.com>
2019-04-05 17:01:37 -0400

    update endpoint for elastic index predictions

--- yelp_data/upload_elastic_predictions.sh
+++ yelp_data/upload_elastic_predictions.sh
@@ -1,1 +1,1 @@
-curl -XPOST https://search-restaurants-cozvshffzyeezzf7wcfsnab45u.us-east-1.es.amazonaws.com/_bulk --data-binary @predictions.json -H 'Content-Type: application/json'
+curl -XPOST https://search-predictions-nxhiwrjp3ljnqsel5gr6sbxsau.us-east-1.es.amazonaws.com/_bulk --data-binary @predictions.json -H 'Content-Type: application/json'
